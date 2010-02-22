bb23b4c81c5566a3542c11e4eb9893678ed8e1a9
chris.sentelle <chris.sentelle@ebffe4ae-1071-11df-96eb-c71931f3db84>
2010-02-22 02:30:26 +0000

    Perform code cleanup, lowering memory consumption and reducing operations.

--- SimplexSVM Mex/SimplexSVM/SVM.h
+++ SimplexSVM Mex/SimplexSVM/SVM.h
@@ -29,16 +29,16 @@ private:
     Array<double, 1>& updateCache(Array<double, 1>& fcache,
                                   const Array<double, 1>& T,
 								  const Array<double, 1>& alpha,
-								  const Array<double, 1>& Pr,
+								  const double beta,
                                   const Array<double, 1>& alphaOld,
-                                  const Array<double, 1>& prOld,
+                                  const double betaOld,
                                   const int pivotIdx);
 
     void reinitializeCache(Array<double, 1>& fcache,
                            Array<double, 1>& upperfcache,
                             const Array<double, 1>& T,
 						    const Array<double, 1>& alpha,
-						    const Array<double, 1>& Pr);
+						    const double beta);
 
 
     // Add a row/column to the factorization (L)
@@ -56,7 +56,7 @@ private:
                   const int idx,
                   Array<double, 1>& fcache,
                   const Array<double, 1>& T,
-                  Array<double, 1>& Pr,
+                  double& beta,
                   Array<double, 1>& uppperfcache,
 				  int& iter);
 
@@ -64,7 +64,7 @@ private:
     int updateCacheStrategy(const int nWorkingSize, 
                              const Array<double, 1>& T,
                              const Array<double, 1>& alpha, 
-                             const Array<double, 1>& Pr,
+                             const double beta,
                              Array<double, 1>& fcache,
                              Array<double, 1>& upperfcache,
                              double& ming);
@@ -88,18 +88,24 @@ private:
 
     Array<double, 2> m_R;
 
-    Array<double, 1> m_t1;
-    Array<double, 1> m_t2;
-    Array<double, 1> m_t3;
 
+	//
+	// These represent arrays that can be preallocated and referenced by the 
+	// actual arrays that grow/shrink in size. This addresses a limitation in
+	// the Blitz arrays where dynamic memory allocation would occur each time
+	// shrinking or growing occurs
+	//
     Array<double, 2> m_RStorage;
-    Array<double, 1> m_gS;
-    Array<double, 1> m_htS;
     Array<double, 1> m_hS;
     Array<double, 1> m_etS;
     Array<double, 1> m_qS;
-    Array<double, 1> m_eS;
-    Array<double, 1> m_qaS;
 
+	//
+	// The m_RStorage array is not preallocated to the maximum size since
+	// storage is O(N^2) spatial complexity. Instead, m_RStorage is preallocated
+	// in chunks, similar to the manner in which the STL library would implement 
+	// this. The following defines how many entries should be added each time
+	// a dynamic allocation occurs.
+	//
     const static int STORAGE_NEW_INCREMENT = 100;
 };
